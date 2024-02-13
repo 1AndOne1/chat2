f47bf9c488307d1a5759ad2018bddc0966054055
1AndOne1 <112853915+1AndOne1@users.noreply.github.com>
2024-02-13 09:34:45 +0500

    Add files via upload

---
+++ chat/host/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using chat;
+using System.ServiceModel.Description;
+
+namespace host
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+         {
+            using(var host = new ServiceHost(typeof(chat.ServiceChat)))
+            {
+                host.Open();
+                Console.WriteLine("Host Start");
+                Console.ReadLine();
+            }
+         }
+    }
+}
